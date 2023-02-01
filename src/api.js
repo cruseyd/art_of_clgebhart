8381e2c2c7b15f52b5816add2d55863f4d5c938b
cgebhart-lbl <cgebhart@lbl.gov>
2023-01-31 16:14:23 -0800

    Trying to print current directory

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     const filenames = fs.readdirSync('/images');
-    res.send(filenames);
+    res.send(__dirname);
 });
 
 app.use('/.netlify/functions/api', router);

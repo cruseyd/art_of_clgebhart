0aeb6a1c96167d6a98f062f227f03e22d39ffa79
cgebhart-lbl <cgebhart@lbl.gov>
2023-01-31 16:36:38 -0800

    Revert "Trying to read filenames"
    This reverts commit 46f44f3750b4373c30ab28e80ab5a66c7fa80fd9.

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const fs = require('fs');
+
 const app = express();
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const filenames = fs.readdirSync('./images');
-    res.send(filenames);
+    res.json({
+        'hello': 'hi!'
+    });
 });
 
 app.use('/.netlify/functions/api', router);
